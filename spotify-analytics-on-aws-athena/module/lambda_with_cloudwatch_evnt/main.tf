128cc2a6319402d27bedf8a6c9e47599d37f9ec5
DE Stuff <91672788+KiranGunturu@users.noreply.github.com>
2024-03-18 05:35:46 +0000

    lambda with cloudwatch and s3 evnt

---
+++ spotify-analytics-on-aws-athena/module/lambda_with_cloudwatch_evnt/main.tf
@@ -0,0 +1,172 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_policy_attachment" "s3_full_access" {
+  name       = "s3_full_access"
+  roles      = [aws_iam_role.iam_for_lambda.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+variable "lambda_function_name" {
+    description = "name of the lambda funciton"
+    default = null
+  
+}
+
+variable "lambda_handler_name" {
+    description = "name of the lambda function handler"
+    default = null
+  
+}
+
+variable "lambda_timeout" {
+    description = "value of the lambda timeout in secs"
+    default = 3
+  
+}
+
+variable "lambda_file_to_be_uploaded" {
+    description = "name of the zip file to be uploaded"
+    default = "test.zip"
+  
+}
+
+variable "lambda_runtime" {
+    description = "value of the lambda runtime"
+    default = "python3.2"
+  
+}
+
+variable "lambda_memory_size" {
+    description = "size of the memory required for lambda function"
+    default = 128
+  
+}
+
+variable "client_id_env_var" {
+    description = "value of the spotify client id"
+    default = null
+
+  
+}
+
+variable "client_secret_env_var" {
+    description = "value of the spotify client id"
+    default = null
+
+}
+
+variable "source_code_hash" {
+    description = "value of the source_code_hash to address code change and deployment"
+  
+}
+
+variable "lambda_spotipy_layer_zip_file" {
+    description = "file name of the lambda layer that we want to create the layer as"
+  
+}
+
+variable "lambda_spotipy_layer_name" {
+    description = "name of the lambda layer name"
+  
+}
+
+variable "lambda_layer_runtime" {
+    description = "value of the lambda layer runtime"
+  
+}
+
+variable "s3_bucket" {
+  description = "name of the s3 bucket where we want lambda to store the data"
+  
+}
+
+variable "s3_key" {
+  description = "name of the s3 folder where we want to lambda to store to the files"
+  
+}
+
+variable "pandas_layer" {
+  description = "name of the aws provided pandas layer"
+  default = null
+  
+}
+resource "aws_lambda_layer_version" "spotipy_layer" {
+  filename   = var.lambda_spotipy_layer_zip_file
+  layer_name = var.lambda_spotipy_layer_name
+  compatible_runtimes = [var.lambda_layer_runtime]
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = var.lambda_file_to_be_uploaded
+  function_name = var.lambda_function_name
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = var.lambda_handler_name
+  timeout = var.lambda_timeout
+  runtime = var.lambda_runtime
+  memory_size = var.lambda_memory_size
+  source_code_hash = var.source_code_hash
+
+
+  environment {
+    variables = {
+      client_id = var.client_id_env_var
+      client_secret = var.client_secret_env_var
+      s3_bucket = var.s3_bucket
+      s3_key = var.s3_key
+    }
+  }
+
+  #layers = [aws_lambda_layer_version.spotipy_layer.arn,"arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:layer:${var.pandas_layer}:17"]
+  layers = [aws_lambda_layer_version.spotipy_layer.arn]
+
+}
+
+
+resource "aws_cloudwatch_event_rule" "lambda_schedule" {
+  name = "lambda_every_one_minute"
+  // Worked after I added `depends_on`
+  depends_on = [
+    aws_lambda_function.test_lambda
+  ]
+  schedule_expression = "rate(1 minute)"
+}
+
+resource "aws_cloudwatch_event_target" "onemin_lambda" {
+  target_id = "onemin_lambda" #I added `target_id`
+  rule = "${aws_cloudwatch_event_rule.lambda_schedule.name}"
+  arn = "${aws_lambda_function.test_lambda.arn}"
+}
+
+resource "aws_lambda_permission" "lambda_every_one_minute" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.test_lambda.function_name}"
+  principal = "events.amazonaws.com"
+  source_arn = "${aws_cloudwatch_event_rule.lambda_schedule.arn}"
+}
---
+++ spotify-analytics-on-aws-athena/transform_spotify_data_s3.py
@@ -0,0 +1,140 @@
+import json
+import boto3
+import pandas as pd
+import datetime
+from io import StringIO
+
+
+# push to s3
+def put_object(client, bucket, key, data):
+    client.put_object(
+        Bucket = bucket,
+        Key = key,
+        Body = data
+        )
+
+def get_current_datetime():
+    current_datetime = datetime.datetime.now()
+    current_datetime_str = current_datetime.strftime("%Y-%m-%d-%H-%M-%S")
+    return current_datetime_str
+    
+def write_to_s3(Bucket, s3_path, df, client):
+    dateAndTime = get_current_datetime()
+    key = s3_path +"_" + dateAndTime + ".csv"
+    data_buffer = StringIO()
+    df.to_csv(data_buffer)
+    content = data_buffer.getvalue()
+    put_object(client, Bucket, key, content)
+
+
+def get_album_data(data):
+    album_data = []
+    for row in data['items']:
+        album_id=row['track']['album']['id']
+        album_name=row['track']['album']['name']
+        album_release_date=row['track']['album']['release_date']
+        album_total_tracks=row['track']['album']['total_tracks']
+        #album=row['track']['album']['artists'][0]['external_urls']['spotify']
+        album_url=row['track']['album']['external_urls']['spotify']   
+        #items=json.dumps(items, indent=3)
+        album = {"album_id": album_id, "album_name": album_name, "release_date": album_release_date, "album_total_tracks": album_total_tracks, "album_url": album_url}
+        album_data.append(album)
+    return album_data
+
+def get_artists_data(data):
+    #print(type(data))
+    #for key, value in data.items():
+        #print(key)
+        #print(f"Data type of value for key '{key}': {type(value)}")
+    #items = data['items'][3]['track']['artists'] # one song/track can have multiple artists so below for loop, loops through all such multiple artists for a track.
+    #items = data['items'][3]['track']['artists']
+    artist_list = []
+    for row in data['items']:
+        for key, value in row.items():
+            if key == 'track':
+                for artist in value['artists']:
+                    artist_dict = {'artist_id': artist['id'], 'artist_name': artist['name'], 'external_url': artist['href']}
+                    artist_list.append(artist_dict)
+    
+
+    return artist_list
+    
+
+def get_songs_data(data):
+    songs_list = []
+    for row in data['items']:
+        song_id = row['track']['id']
+        song_name = row['track']['name']
+        duration_ms =  row['track']['duration_ms']
+        song_url = row['track']['external_urls']['spotify']
+        song_popularity = row['track']['popularity']
+        song_added = row['added_at']
+        album_id = row['track']['album']['id']
+        artist_id = row['track']['album']['artists'][0]['id']
+        songs_info = {'song_id': song_id, 'song_name': song_name, 'duration_ms': duration_ms, 'song_url': song_url, 'song_popularity': song_popularity,
+                'song_added': song_added, 'album_id': album_id, 'artist_id': artist_id}
+        songs_list.append(songs_info)
+    return songs_list
+
+def transform_spotify_data_to_s3(event, context):
+    Bucket = "spotify-analytics-raw-data"
+    Key = "raw_data/landing/"
+    client = boto3.client('s3')
+    spotify_data = []
+    spotify_keys = []
+    for file in client.list_objects(Bucket=Bucket, Prefix=Key)['Contents']:
+        print(file['Key'])
+        file_key = file['Key']
+        if file_key.split(".")[-1] == "json":
+            response = client.get_object(Bucket=Bucket, Key=file_key)
+            content = response['Body']
+            jsonData = json.loads(content.read())
+            print(jsonData)
+            spotify_data.append(jsonData)
+            spotify_keys.append(file_key)
+    
+    for data in spotify_data:
+        albums = get_album_data(data)
+        artists = get_artists_data(data)
+        songs = get_songs_data(data)
+        
+        # create album dataframe
+        album_Df = pd.DataFrame.from_dict(albums)
+        #print(album_Df.head())
+        album_Df = album_Df.drop_duplicates(subset=['album_id'])
+        
+        
+        # create artists dataframe
+        artsits_Df = pd.DataFrame.from_dict(artists)
+        #print(artsits_Df.head())
+        artsits_Df = artsits_Df.drop_duplicates(subset=['artist_id'])
+        
+        
+        # create songs dataframe
+        songs_Df = pd.DataFrame.from_dict(songs)
+        songs_Df = songs_Df.drop_duplicates(subset=['song_id'])
+        
+        # write songs data to s3
+        s3_path = "transformed_data/songs/songs_transformed"
+        write_to_s3(Bucket, s3_path, songs_Df, client)
+        
+        # write artists data to s3
+        s3_path = "transformed_data/artists/artists_transformed"
+        write_to_s3(Bucket, s3_path, artsits_Df, client)
+        
+          # write albums data to s3
+        s3_path = "transformed_data/albums/albums_transformed"
+        write_to_s3(Bucket, s3_path, album_Df, client)
+    
+    # file archive
+    s3_resource = boto3.resource('s3')
+    for key in spotify_keys:
+        copy_source = {
+            'Bucket': Bucket,
+            'Key': key
+        }
+        s3_resource.meta.client.copy(copy_source, Bucket, 'raw_data/archive/' + key.split("/")[-1])
+        s3_resource.Object(Bucket, key).delete()
+        
+        
+    
