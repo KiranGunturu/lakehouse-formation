b4796b3a760441bb1c1b27177cf8813cc0409a24
DE Stuff <91672788+KiranGunturu@users.noreply.github.com>
2024-03-18 05:48:48 +0000

    lambda exe time changed to 300 secs

--- spotify-analytics-on-aws-athena/provision-lambda/lambda.tf
+++ spotify-analytics-on-aws-athena/provision-lambda/lambda.tf
@@ -12,7 +12,7 @@ module "lambda-raw" {
     lambda_file_to_be_uploaded = "/workspaces/terraform/spotify-analytics-on-aws-athena/spotify_extraxt_raw_data.zip"
     lambda_function_name = "spotify_extract_raw_data"
     lambda_handler_name = "extract_from_spotify.extract_and_ingest_to_s3"
-    lambda_timeout = 60
+    lambda_timeout = 300
     lambda_runtime = "python3.8"
     lambda_memory_size = 128
     source_code_hash = filebase64sha256("/workspaces/terraform/spotify-analytics-on-aws-athena/spotify_extraxt_raw_data.zip")
@@ -34,7 +34,7 @@ module "lambda-transform" {
     lambda_file_to_be_uploaded = "/workspaces/terraform/spotify-analytics-on-aws-athena/transform_spotify_data_s3.zip"
     lambda_function_name = "spotify_transform_raw_data"
     lambda_handler_name = "transform_spotify_data_s3.transform_spotify_data_to_s3"
-    lambda_timeout = 60
+    lambda_timeout = 300
     lambda_runtime = "python3.8"
     lambda_memory_size = 128
     source_code_hash = filebase64sha256("/workspaces/terraform/spotify-analytics-on-aws-athena/transform_spotify_data_s3.zip")
