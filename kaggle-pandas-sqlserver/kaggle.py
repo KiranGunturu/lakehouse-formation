af62da4f798b84157edf008ffb3523c1e88d1fe1
DE Stuff <91672788+KiranGunturu@users.noreply.github.com>
2024-04-15 17:17:17 +0000

    data cleaning

--- kaggle-pandas-sqlserver/kaggle.py
+++ kaggle-pandas-sqlserver/kaggle.py
@@ -1,7 +1,10 @@
 import kaggle
-import zipfile
+import zipfile36=0.1.3
 import pandas as pd
 import numpy as np
+import sqlalchemy as sal
+import boto3
+from io import StringIO
 
 #!kaggle datasets download kirangunturu01/retail-orders -f orders.csv
 
@@ -20,13 +23,19 @@ def read_csv(file):
     df = pd.read_csv(file)
     return df
 
-if __name__ == "__main__":
+def ingest_to_sqlserver(df):
+    # create the sql server connection
+    engine = sal.create_engine('mssql://KGUNTURU/SQLEXPRESS/master?driver=ODBC+DRIVER+17+FOR+SQL+SERVER')
+    conn = engine.connect()
 
-    # unzip
-    unzip_csv(zipfilepath)
+    # load the data to sql server with replace the table if exists. drop and create the table
+    #df.to_sql('orders', con=conn, index=False, if_exists = 'replace') # cons: pandas will create highest possible datatype like varchar(max)
 
-    # read data from the file and handle null values in the dataset
-    df = read_csv(file)
+
+    # load the data to sql server with append option. table should exist first
+    df.to_sql('orders', con=conn, index=False, if_exists = 'append')
+
+def data_cleaning(df):
     print(df.head(10))
     print(list(df['Ship Mode'].unique())) # ['Second Class', 'Standard Class', 'Not Available', 'unknown', 'First Class', nan, 'Same Day']
 
@@ -38,6 +47,49 @@ if __name__ == "__main__":
     df.columns = df.columns.str.lower()
     df.columns = df.columns.str.replace(' ', '_')
     print(df.columns)
+    print(df.head(3))
+
+    # create discount column, sale price and profit columns
+    df['discount'] = df['cost_price']*df['discount_percent']*.01
+    df['sale_price'] = df['list_price']-df['discount']
+    df['profit'] = df['sale_price']-df['cost_price']
+    print(df.head(3))
+    print(df.dtypes)
+
+    # convert order date from object datatype to datetime
+    df['order_date'] = pd.to_datetime(df['order_date'], format="%Y-%m-%d")
+    print(df.dtypes)
+
+    # drop cost_price, list_price and discount_percent columns
+    df.drop(columns=['cost_price', 'list_price','discount_percent'], inplace=True)
+    print(df.head(3))
+    return df
+
+def push_to_s3(df):
+
+
+
+
+
+if __name__ == "__main__":
+
+    # unzip
+    unzip_csv(zipfilepath)
+
+    # read data from the file and handle null values in the dataset
+    df = read_csv(file)
+
+    # data cleaning
+    df =  data_cleaning(df)
+
+
+    
+
+    
+
+
+
+    
     
 
 
